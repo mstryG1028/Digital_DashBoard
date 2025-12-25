dd0494e552e83a0b8fb52152fa730629828e410f
deepak mistry <mistrydeepak1028@gmail.com>
2025-12-25 14:45:37 +0530

    Ready for Render deployment

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ mongoose.connect(process.env.MONGO_URL, {
 
 app.use(
   session({
-    secret: "secret-key",
+    secret: process.env.SESSION_SECRET|| "secret-key",
     resave: false,
     saveUninitialized: false,
   })
